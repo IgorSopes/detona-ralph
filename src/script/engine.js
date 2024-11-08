e8c6ebd252ecde411bef429f55543f623e9f433d
IgorSopes <sopesigor@gmail.com>
2024-11-07 21:27:26 -0300

    add função js

---
+++ src/script/engine.js
@@ -0,0 +1,28 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {},
+};
+
+function randomSquare() {
+    state.view.squares.forEach
+}
+function addListenerHitBox  () {
+    state.view.squares.forEach ((square) => {
+        square.classList.remove("enemy");
+    });
+    let randomNumber = Math.floor(Math.random()* 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList("enemy");
+}
+
+function initialize() {
+    alert("começou");
+    randomSquare();
+}
+
+initialize();
